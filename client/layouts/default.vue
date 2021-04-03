55a954be508f5f7d536fe1157255b8b7eb205f8d
Bektur Muratov <prostreyd@gmail.com>
2021-04-03 11:07:40 +0600

    first commit

---
+++ client/layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="app">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<style>
+html {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.app {
+  /* background-color: #009688; */
+  background-image: -webkit-linear-gradient(
+    180deg,
+    #009688 0,
+    #009688 127px,
+    #dddbd1 127px,
+    #dddbd1 127px
+  );
+  background-image: linear-gradient(
+    180deg,
+    #009688 0,
+    #009688 127px,
+    #dddbd1 127px,
+    #dddbd1 127px
+  );
+  min-height: 100vh;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
---
+++ client/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+    <div class="2xl:py-6 h-screen flex justify-center">
+      <div class="h-full max-w-screen-2xl w-full bg-gray-900">
+        <h1 class="title">client</h1>
+        <div class="links"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.main {
+  margin: 0 auto;
+  text-align: center;
+  min-height: 100vh;
+  /* display: flex;
+  justify-content: center;
+  max-width: 1400px; */
+}
+</style>
